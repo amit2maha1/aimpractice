e570b08909476251300777f8dca7402bbd798b1c
amit <amit2maha1@gmail.com>
2019-07-13 21:14:12 +0545

    Updates

---
+++ precache-manifest.47efc7ba3be4f966c7224888cddaa5b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6beadc56edf9849fc5685c32aeb6ed9",
+    "url": "/aimpractice/index.html"
+  },
+  {
+    "revision": "53e337b0bacf890b39f7",
+    "url": "/aimpractice/static/css/main.de65dc68.chunk.css"
+  },
+  {
+    "revision": "c343ce630bce0c5198ca",
+    "url": "/aimpractice/static/js/2.e09f9078.chunk.js"
+  },
+  {
+    "revision": "53e337b0bacf890b39f7",
+    "url": "/aimpractice/static/js/main.5115bc35.chunk.js"
+  },
+  {
+    "revision": "38d0ef9297c2454ff3c7",
+    "url": "/aimpractice/static/js/runtime~main.17ae833d.js"
+  },
+  {
+    "revision": "2d4fe112a20f0efca2ec369c3fee942e",
+    "url": "/aimpractice/static/media/crosshair.2d4fe112.gif"
+  },
+  {
+    "revision": "f2fe9ce06d6cdbf315ae5064bfe5fda4",
+    "url": "/aimpractice/static/media/terrorist.f2fe9ce0.png"
+  }
+]);
